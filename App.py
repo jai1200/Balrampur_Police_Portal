ba398263260676620cb454a46e2a26ccc283d4d1
abhishek-b4s~ <abhishek.srivastav@buddy4study.com>
2026-06-08 09:49:41 +0530

    Third Portion removed

--- App.py
+++ App.py
@@ -216,7 +216,7 @@ else:
     # === मुख्यालय मास्टर व्यू ===
     if st.session_state.user_role == "hq_master":
         st.markdown("<h2 style='color:#000000;'>📊 मुख्यालय मॉनिटरिंग डैशबोर्ड (Master Page)</h2>", unsafe_allow_html=True)
-        tab1, tab2, tab3 = st.tabs(["📋 लाइव ड्यूटी मॉनिटर", "👮 कर्मी विवरण एवं स्मार्ट सर्च", "🎯 स्वचालित रैंडम ड्यूटी अलॉटमेंट"])
+        tab1, tab2, tab3 = st.tabs(["📋 लाइव ड्यूटी मॉनिटर", "👮 कर्मी विवरण एवं स्मार्ट सर्च"]) #"🎯 स्वचालित रैंडम ड्यूटी अलॉटमेंट"
         
         with tab1:
             col1, col2, col3 = st.columns(3)
